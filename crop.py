463eca2ff34aba8a9c8478b3ee35d55ac62556af
niaki <nina.zizakic@ugent.be>
2021-06-29 13:08:59 +0200

    Make a script for cropping an image

--- crop.py
+++ crop.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
+
 import argparse
-import numpy as np
 import imageio
 
 
@@ -9,39 +10,22 @@ parser.add_argument('input', default='image.png',
 parser.add_argument('--output',
                     help="Path to the output image to be saved")
 
-parser.add_argument('--h1', default='0',
-                    help="Height coordinate begin")
-parser.add_argument('--w1', default='0',
-                    help="Width coordinate begin")
 
-parser.add_argument('--h2', default='10',
-                    help="Height coordinate end")
-parser.add_argument('--w2', default='20',
-                    help="Width coordinate end")
-
-parser.add_argument('--rgb', default='1',
-                    help="Is the image RGB? (1 for yes, 0 for no)")
-
+parser.add_argument('--he', default=['0', '10'], nargs=2, type=int,
+                    help="Height coordinates")
+parser.add_argument('--wi', default=['0', '20'], nargs=2, type=int,
+                    help="Width coordinates")
 
 args = parser.parse_args()
 
 image = imageio.imread(args.input)
 
 if args.output is None:
-    # args.output = args.input[: args.input.rfind(.) - 1] + '_crop' + args.input[args.input.rfind(.) - 1 :]
     args.output = args.input[: args.input.rfind('.')] + '_crop' + args.input[args.input.rfind('.') :]
 
-
-
-h1 = int(args.h1)
-w1 = int(args.w1)
-h2 = int(args.h2)
-w2 = int(args.w2)
-rgb = int(args.rgb)
-
-if rgb:
-    image_crop = image[h1:h2, w1:w2, :]
+if len(image.shape) == 3:
+    image_crop = image[args.he[0]:args.he[1], args.wi[0]:args.wi[1], :]
 else:
-    image_crop = image[h1:h2, w1:w2]
+    image_crop = image[args.he[0]:args.he[1], args.wi[0]:args.wi[1]]
 
 imageio.imsave(args.output, image_crop)

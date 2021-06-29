89403fd3d5b38a64f797d248971d58dc602763cd
niaki <nina.zizakic@ugent.be>
2021-06-29 12:34:52 +0200

    Make a script for cropping an image (WIP2)

--- crop.py
+++ crop.py
@@ -4,10 +4,10 @@ import imageio
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', default='image.png',
+parser.add_argument('input', default='image.png',
                     help="Path to the input image to be cropped")
-parser.add_argument('--output', default='image_crop.png',
-                    help="Path to the output image to be saved")  # TODO make it only an addition to the original name
+parser.add_argument('--output',
+                    help="Path to the output image to be saved")
 
 parser.add_argument('--h1', default='0',
                     help="Height coordinate begin")
@@ -26,6 +26,12 @@ parser.add_argument('--rgb', default='1',
 args = parser.parse_args()
 
 image = imageio.imread(args.input)
+
+if args.output is None:
+    # args.output = args.input[: args.input.rfind(.) - 1] + '_crop' + args.input[args.input.rfind(.) - 1 :]
+    args.output = args.input[: args.input.rfind('.')] + '_crop' + args.input[args.input.rfind('.') :]
+
+
 
 h1 = int(args.h1)
 w1 = int(args.w1)
